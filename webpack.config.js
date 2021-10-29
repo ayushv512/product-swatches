900d591eef8164c737466edced34ba478de4fbd5
Suraj Pasayat <suraj.93p@gmail.com>
2021-10-29 09:49:33 +0800

    hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
+  mode:"development",
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -37,7 +38,10 @@ const config = {
       templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>Flipkart</title><link rel="icon" href="https://static-assets-web.flixcart.com/www/promos/new/20150528-140547-favicon-retina.ico" sizes="32x32"></head><body><div id=\"app\"></div></body></html>',
       filename: 'index.html',
     }),
-  ]
+  ],
+  devServer: {
+    open: true
+  }
 };
 
 module.exports = config;
